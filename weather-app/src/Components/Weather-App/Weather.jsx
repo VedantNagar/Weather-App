aa9b66444598e3ee745b8bcd7b48ab47a78e18b3
Vedant <vedantnagar21@gmail.com>
2024-05-07 17:40:26 +0530

    added CSS

--- weather-app/src/Components/Weather-App/Weather.jsx
+++ weather-app/src/Components/Weather-App/Weather.jsx
@@ -19,6 +19,27 @@ const Weather = () => {
             <img src={search_icon} alt='search-icon' />
           </div>
         </div>
+        <div className='weather-image'>
+          <img src={cloud} alt='clouds' />
+        </div>
+        <div className='weather-temp'>24°C</div>
+        <div className='weather-location'>New Delhi</div>
+        <div className='data-container'>
+          <div className='element'>
+            <img src={humidity} alt='' className='icon' />
+            <div className='data'>
+              <div className='humidity'>69%</div>
+              <div className='text'>Humidity</div>
+            </div>
+          </div>
+          <div className='element'>
+            <img src={wind} alt='' className='icon' />
+            <div className='data'>
+              <div className='wind-speed'>69 km/hr</div>
+              <div className='text'>Wind Speed</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
